34af14a8f88cc649983a514c5c602a2acac63e77
TejodhayBonam <bonamtejodhay@gmail.com>
2023-07-27 04:33:57 -0400

    VRWanderWorld

---
+++ README.md
@@ -0,0 +1,55 @@
+ # 04/06/23 RR12 Ramble with Rami: Part 1 - Stroll and Groove 🏝🗽🥾🎒
+
+<img src ="./assets/banner1.png" alt="Ramble with Rami Banner" width="100%"/>
+
+Welcome to Stroll & Groove, an immersive platform that allows you to virtually experience walking through different cities around the world while enjoying local sounds and music. 🌍🎧
+
+## Project Overview
+
+Stroll & Groove is a project that aims to provide users with an engaging and interactive way to explore cities on foot. By leveraging street view technology and curated audio content, users can embark on virtual walks through various cities, experiencing the sights, sounds, and atmosphere of different locations.
+
+## Features
+
+- **Virtual City Walks**: 🏞️ Take virtual walks through iconic cities worldwide and immerse yourself in the local environment.
+- **Realistic Street Views**: 🌆 Experience realistic street views to get a sense of the city's architecture, landmarks, and streetscapes.
+- **Curated Audio Content**: 🎵 Listen to a variety of local sounds, music, and ambient audio that captures the spirit of each city. (Over 50 local sounds/radios available, with more to be added)
+- **Interactive Experience**: 🕹️ Navigate through the city streets using intuitive controls and explore different neighborhoods and attractions. (Please note that there may be some buffering, so please wait a few seconds)
+- **City Selection**: 🌇 Choose from a diverse range of cities to stroll through and discover their unique vibes. (My personal recommendation: Start with Budapest!)
+
+## Inspiration
+
+Stroll & Groove was born out of the desire to explore new places and experience past adventures during lockdown. As a student preparing for university finals, I found myself missing out on the opportunity to travel to unique destinations. This project became my way of bringing the world to me, helping me study at home while virtually exploring different cities. I wanted to create an interactive and immersive experience that would transport users to exciting destinations and provide a much-needed break from everyday routines.
+
+## Contributing
+
+Contributions to Stroll & Groove are welcome! If you would like to contribute to the project, please follow these steps:
+
+1. Fork the repository on GitHub.
+2. Create a new branch with a descriptive name for your feature or bug fix.
+3. Implement your changes, following the project's coding style and guidelines.
+4. Commit and push your changes to your forked repository.
+5. Submit a pull request, explaining your changes and the benefits they bring.
+
+## License
+
+Stroll & Groove is a project licensed under the MIT License. 
+
+## Acknowledgements
+
+Stroll & Groove was inspired by the popular 'Drive & Listen' project, which allowed users to explore the world from the comfort of their homes. 🚶🎶
+
+## Show Your Support 🤝
+
+<b> Thank you </b> for taking the time to drop by and check out this project  ☺️
+
+If you've liked or found this helpful, please consider giving it a star 🌟 and supporting me on my journey to improve my programming skills and chase my aspirations. I'd love to hear your feedback on this project, so feel free to leave your thoughts and suggestions 📬
+
+Every contribution counts and is deeply appreciated as I pursue a path of lifelong learning and aim to share meaningful content for the <b> travellers, daydreamers, and those that crave a taste of the world's vibrant melodies </b> 🌎
+
+Thank you again for your support! 🙏
+
+---
+
+I hope you enjoy using Stroll & Groove and that it provides you with a unique and immersive way to explore places around the world. Happy strolling and grooving! 🥾🎒
+
+Further recommendations are welcome! Let me know which places you'd like to see next 🌃
---
+++ js/locations.js
@@ -0,0 +1,574 @@
+LOCATIONS = [ 
+	{
+		"id": 23,
+		"name": "Accra, Ghana",
+		"videoId": "VXUxkr9OsiI",
+		"radio": {
+			"url": "https://citi973fm.radioca.st/stream/1/",
+			"name": "Citi FM"
+		},
+		"startTime": 80
+	},
+
+	{
+        "id": 37,
+        "name": "Athens, Greece",
+        "videoId": "W1EJY_CFtWs",
+        "radio": {
+            "url": "https://stream.vanillaradio.com:8012/live",
+            "name": "Vanilla Radio"
+        },
+        "startTime": 467
+    },
+	
+	{
+        "id": 31,
+        "name": "Barcelona, Spain",
+        "videoId": "3N4oFYMJvH8",
+        "radio": {
+            "url": "https://21313.live.streamtheworld.com/RAC_1_SC",
+            "name": "RAC 1"
+        },
+        "startTime": 11
+    },
+
+    {
+        "id": 50,
+        "name": "Berlin, Germany",
+        "videoId": "zca688Ecwpk",
+        "radio": {
+            "url": "https://topradio-de-hz-fal-stream08-cluster01.radiohost.de/kissfm_mp3-192",
+            "name": "Kiss FM Radio"
+        },
+        "startTime": 43
+    },
+
+	{
+		"id": 3,
+		"name": "Beverly Hills, California",
+		"videoId": "ybnQcnWssWg",
+		"radio": {
+			"url": "http://s18.myradiostream.com:9676/;stream.nsv",
+			"name": "NonStopOldies"
+		},
+		"startTime": 13
+	},
+	{
+        "id": 36,
+        "name": "Budapest, Hungary",
+        "videoId": "XJDVn1WZRSw",
+        "radio": {
+            "url": "http://stream1.bestfmbudapest.hu/bestfm_nyiregyhaza.mp3",
+            "name": "v"
+        },
+        "startTime": 280
+    },
+
+	{
+        "id": 24,
+        "name": "Buenos Aires, Argentina",
+        "videoId": "Pafh8WxKPrU",
+        "radio": {
+            "url": "https://stream-159.zeno.fm/60pqgs97f2zuv?zs=zvNvBBGMTfaN4gmcSELthg",
+            "name": "Planeta Cumbia Latina"
+        },
+        "startTime": 840
+    },
+
+    {
+        "id": 10,
+        "name": "Brno, Czech Republic",
+        "videoId": "BGRhCDKz6lQ",
+        "radio": {
+            "url": "https://ice.abradio.cz/magicbrno128.mp3",
+            "name": "Hitrádio City"
+        },
+        "startTime": 8
+    },
+
+	{
+        "id": 40,
+        "name": "Chicago, Illinois, U.S",
+        "videoId": "5alwy_vLWUM",
+        "radio": {
+            "url": "https://stream.wbez.org/wbez64-web.aac",
+            "name": "Chicago Public Radio"
+        },
+        "startTime": 45
+    },
+
+	{
+        "id": 13,
+        "name": "Copenhagen, Denmark",
+        "videoId": "AQqPG14QjJM",
+        "radio": {
+            "url": "http://live-icy.gss.dr.dk/A/A08L.mp3",
+            "name": "DR P4 København"
+        },
+        "startTime": 18
+    },
+
+	{
+        "id": 21,
+        "name": "Cyberpunk, 2077",
+        "videoId": "b_rwSMbd1Vk",
+        "radio": {
+            "url": "https://coderadio-admin.freecodecamp.org/radio/8010/radio.mp3",
+            "name": "Code Radio"
+        },
+        "startTime": 10
+    },
+
+
+	{
+        "id": 12,
+        "name": "Downtown New York City, New York, U.S",
+        "videoId": "KJbHcb5ZI_4",
+        "radio": {
+            "url": "https://live.powerhitz.com/hot108?aw_0_req.gdpr=true",
+            "name": "Hot 108 Jamz"
+        },
+        "startTime": 30
+    },
+
+	{
+		"id": 7,
+		"name": "Dubai, United Arab Emirates",
+		"videoId": "JXe-1dIlCw4",
+		"radio": {
+			"url": "https://au1.fastcast4u.com/proxy/dzcwbfih_relay?mp=/1",
+			"name": "1062 BIG FM"
+		},
+		"startTime": 19
+	},
+
+    {
+        "id": 48,
+        "name": "Dubrovnik, Croatia",
+        "videoId": "DGDqwjQDwUQ",
+        "radio": {
+            "url": "https://s5.radio.co/s8cf5effc6/listen",
+            "name": "Radio More"
+        },
+        "startTime": 390
+    },
+
+	{
+		"id": 9,
+		"name": "Disney Magic Kingdom, Florida, U.S",
+		"videoId": "M-3cS51G6oQ",
+		"radio": {
+			"url": "https://playerservices.streamtheworld.com/api/livestream-redirect/977_HITSAAC_SC",
+			"name": ".977 Today's Hits"
+		},
+		"startTime": 70
+	},
+	{
+		"id": 4,
+		"name": "Florence, Italy",
+		"videoId": "AqFLqzHAgzA",
+		"radio": {
+			"url": "https://ice08.fluidstream.net/ibiza.aac",
+			"name": "Radio Ibiza"
+		},
+		"startTime": 91
+	},
+
+    {
+        "id": 43,
+        "name": "Hanoi, Vietnam",
+        "videoId": "nbQ_z-0ZO28",
+        "radio": {
+            "url": "http://rfivietnamien96k.ice.infomaniak.ch/rfivietnamien-96k.mp3",
+            "name": "RFI Tiếng Việt"
+        },
+        "startTime": 65
+    },
+
+	{
+		"id": 6,
+		"name": "Ibiza, Spain",
+		"videoId": "axqSGXU5qIw",
+		"radio": {
+			"url": "https://cadena100-cope-rrcast.flumotion.com/cope/cadena100-low.mp3",
+			"name": "Cadena 100"
+		},
+		"startTime": 18
+	},
+
+	{
+        "id": 30,
+        "name": "Lake Como, Bellagio, Italy",
+        "videoId": "o7XgU4ut6jQ",
+        "radio": {
+            "url": "https://kisskiss.fluidstream.eu/KKItalia.aac?FLID=M",
+            "name": "TeleRadioStereo"
+        },
+        "startTime": 120
+    },
+
+	{
+		"id": 5,
+		"name": "Lisbon, Portugal",
+		"videoId": "m0K29dzyLCc",
+		"radio": {
+			"url": "https://20863.live.streamtheworld.com/RFM_SC",
+			"name": "RFM"
+		},
+		"startTime": 31
+	},
+	{
+		"id": 1,
+		"name": "London, England",
+		"videoId": "MGMqLGvtNpY",
+		"radio": {
+			"url": "https://media-ssl.musicradio.com/Capital",
+			"name": "Capital FM"
+		},
+		"startTime": 31
+	},
+
+	{
+        "id": 16,
+        "name": "Manhattan, New York City, U.S",
+        "videoId": "6y5CqAHxGX0",
+        "radio": {
+            "url": "https://26113.live.streamtheworld.com/WBBRAMAAC48_SC",
+            "name": "Bloomberg New York"
+        },
+        "startTime": 1
+    },
+
+	{
+        "id": 26,
+        "name": "Medellín, Colombia",
+        "videoId": "jgMg2_owl0s",
+        "radio": {
+            "url": "https://22833.live.streamtheworld.com/BLURADIO_ADP_SC?dist=%252520blu_web",
+            "name": "Blu Radio"
+        },
+        "startTime": 56
+    },
+
+	{
+        "id": 17,
+        "name": "Mexico City, Mexico",
+        "videoId": "fVh9YNTzhak",
+        "radio": {
+            "url": "https://stream-155.zeno.fm/2ieszeso9istv?zs=zyGoecKtQSyPKwA3kiPzqA",
+            "name": "RADIO HABANA"
+        },
+        "startTime": 44
+    },
+
+	{
+        "id": 39,
+        "name": "Monte Carlo, Monaco",
+        "videoId": "2vx3utqzZ1I",
+        "radio": {
+            "url": "https://radiomonaco.ice.infomaniak.ch/radiomonaco-128.mp3",
+            "name": "Radio Monaco "
+        },
+        "startTime": 21
+    },
+
+
+	{
+		"id": 11,
+		"name": "Montreal, Canada",
+		"videoId": "poiKJGuLDNo",
+		"radio": {
+			"url": "https://rogers-hls.leanstream.co/rogers/tor590.stream/icy",
+			"name": "The FAN 590"
+		},
+		"startTime": 48
+	},
+
+	{
+        "id": 27,
+        "name": "Moscow, Russia",
+        "videoId": "dtZ2Bn4TO4g",
+        "radio": {
+            "url": "https://nashe1.hostingradio.ru/best-128.mp3",
+            "name": "Best FM Moscow"
+        },
+        "startTime": 10
+    },
+
+    {
+        "id": 45,
+        "name": "Mumbai, India",
+        "videoId": "ItH9EDnsTgw",
+        "radio": {
+            "url": "https://14223.live.streamtheworld.com/CHN_TAM_ESTAAC.aac",
+            "name": "Radio Mirchi"
+        },
+        "startTime": 2
+    },
+
+	{
+        "id": 38,
+        "name": "Mykonos, Greece",
+        "videoId": "Skhl3C-6WL8",
+        "radio": {
+            "url": "http://sfera.live24.gr/sfera4132",
+            "name": "Sfera 102.2"
+        },
+        "startTime": 45
+    },
+
+	{
+        "id": 32,
+        "name": "Nice, France",
+        "videoId": "UNpzuU3H1d0",
+        "radio": {
+            "url": "https://str1.openstream.co/589?aw_0_1st.collectionid%3D3139%26stationId%3D3139%26publisherId%3D613%26k%3D1688630626",
+            "name": "ABC Lounge"
+        },
+        "startTime": 9
+    },
+
+	{
+		"id": 8,
+		"name": "Nuremberg, Germany",
+		"videoId": "P17hbhKcz7o",
+		"radio": {
+			"url": "https://addrad.io/4WRMMN",
+			"name": "Eins Live"
+		},
+		"startTime": 27
+	},
+
+	{
+        "id": 28,
+        "name": "Oslo, Norway",
+        "videoId": "TD6rh0LtWQI",
+        "radio": {
+            "url": "https://p4.p4groupaudio.com/P04_MM",
+            "name": "P4 Lyden av Norge"
+        },
+        "startTime": 5
+    },
+
+	{
+        "id": 29,
+        "name": "Palermo, Sicily",
+        "videoId": "MTG3VA-m6qk",
+        "radio": {
+            "url": "https://stream10.xdevel.com/audio32s975552-1839/stream/icecast.audio",
+            "name": "Radio Cuore Italia"
+        },
+        "startTime": 120
+    },
+
+	{
+        "id": 34,
+        "name": "Prague, Czech Republic",
+        "videoId": "DhyY3Wh3J-8",
+        "radio": {
+            "url": "https://22533.live.streamtheworld.com/FREKVENCE1AAC.aac",
+            "name": "Frekvence 1"
+        },
+        "startTime": 5
+    },
+
+    {
+        "id": 42,
+        "name": "Rethymno, Greece",
+        "videoId": "KxULYONI4wU",
+        "radio": {
+            "url": "http://live.skyradio.gr:9920/;",
+            "name": "Sky Radio"
+        },
+        "startTime": 15
+    },
+
+
+	{
+        "id": 25,
+        "name": "Rio de Janeiro, Brazil",
+        "videoId": "wXzegSoBGFw",
+        "radio": {
+            "url": "https://eu10.fastcast4u.com/iloverio",
+            "name": "I Love Rio Radio"
+        },
+        "startTime": 420
+    },
+
+	{
+		"id": 2,
+		"name": "Rome, Italy",
+		"videoId": "EsFheWkimsU",
+		"radio": {
+			"url": "https://icstream.rds.radio/rds",
+			"name": "RDS"
+		},
+		"startTime": 55
+	},
+
+    {
+        "id": 49,
+        "name": "Saint Petersburg, Russia",
+        "videoId": "KBeCMiUPuic",
+        "radio": {
+            "url": "https://radiorecord.hostingradio.ru/rr_main96.aacp",
+            "name": "Record"
+        },
+        "startTime": 33
+    },
+
+	{
+        "id": 33,
+        "name": "Santorini, Greece",
+        "videoId": "JbH_heoKccs",
+        "radio": {
+            "url": "https://cast2.radiohost.ovh/8000?mp=/stream/",
+            "name": "Rodos.Greece"
+        },
+        "startTime": 71
+    },
+
+	{
+        "id": 19,
+        "name": "Singapore, Republic of Singapore",
+        "videoId": "SYpRb2Pxl6o",
+        "radio": {
+            "url": "https://rdopanel.cobrasoftwares.org:1065/stream",
+            "name": "Naga FM"
+        },
+        "startTime": 63
+    },
+
+	{
+        "id": 20,
+        "name": "Seoul, South Korea",
+        "videoId": "3aas-RT7Ul8",
+        "radio": {
+            "url": "https://stream-149.zeno.fm/nkb20ze9t18uv?zs=WsIRDdxPSlqjIDBEEFn0LQ",
+            "name": "Moon Music"
+        },
+        "startTime": 31
+    },
+
+	{
+        "id": 18,
+        "name": "Tokyo, Japan",
+        "videoId": "0nTO4zSEpOs",
+        "radio": {
+            "url": "https://kathy.torontocast.com:3560/;",
+            "name": "J-Pop Powerplay"
+        },
+        "startTime": 50
+    },
+	
+	{
+        "id": 22,
+        "name": "Sydney, Australia",
+        "videoId": "dK3ih5op1F8",
+        "radio": {
+            "url": "https://21333.live.streamtheworld.com/2GB.mp3",
+            "name": "2GB Sydney"
+        },
+        "startTime": 120
+    },
+
+	{
+        "id": 14,
+        "name": "Ubud, Bali, Indonesia",
+        "videoId": "5p5FoQR8wTM",
+        "radio": {
+            "url": "https://live.phoenixradiobali.com:8250/phoenixradio",
+            "name": "Phoenix Radio Bali"
+        },
+        "startTime": 5
+    },
+
+	{
+        "id": 35,
+        "name": "Valletta, Malta",
+        "videoId": "KPV1hFJIXrg",
+        "radio": {
+            "url": "https://s10.voscast.com:8203/stream",
+            "name": "One Radio"
+        },
+        "startTime": 91
+    },
+
+    {
+        "id": 46,
+        "name": "Vancouver, Canada",
+        "videoId": "lh8dNmneVyY",
+        "radio": {
+            "url": "https://s4.radio.co/s5de101e4f/listen",
+            "name": "Epic Radio"
+        },
+        "startTime": 15
+    },
+
+	{
+        "id": 15,
+        "name": "Venice, Italy",
+        "videoId": "ZNJjMNQnfLc",
+        "radio": {
+            "url": "https://ice45.fluidstream.net/vjr.aac?FLID=8",
+            "name": "Venice Jazz Radio"
+        },
+        "startTime": 3
+    },
+
+    {
+        "id": 41,
+        "name": "Venice Canal Tour, Italy",
+        "videoId": "91q_XSSDCDE",
+        "radio": {
+            "url": "http://sr2.inmystream.info:8040/stream",
+            "name": "Capital Disko"
+        },
+        "startTime": 1
+    },
+
+    {
+        "id": 47,
+        "name": "Wengen, Switzerland",
+        "videoId": "y5SUzYFV2vc",
+        "radio": {
+            "url": "https://stream.srg-ssr.ch/m/rsc_de/mp3_128",
+            "name": "Radio Swiss Classic"
+        },
+        "startTime": 720
+    },
+
+    {
+        "id": 44,
+        "name": "Zhangjiajie, Hunan, China",
+        "videoId": "JV48zMUWC6A",
+        "radio": {
+            "url": "https://lhttp.qingting.fm/live/339/64k.mp3",
+            "name": "北京新闻广播"
+        },
+        "startTime": 45
+    },
+]
+
+function chooseRandomLocation() {
+	const availableLocations = LOCATIONS.filter(({id}) => state.currentLocation ? state.currentLocation.id !== id : true);
+	
+	return availableLocations[Math.floor(Math.random() * availableLocations.length)];
+}
+
+function changeLocation(locationId) {
+	if (state.currentLocation && locationId == state.currentLocation.id)
+		return;
+
+	changeLoadingState(true);
+
+	const location = LOCATIONS.find(location => location.id == locationId);
+
+	state.currentLocation = location;
+
+	changeLocationText(location.name);
+	changeVideoSource(location.videoId);
+	changeRadio(location.radio);
+	toggleActiveClassOnLocation(location.id);
+	playVideo(location);
+}
